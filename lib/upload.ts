b31753b3608e63f8b658770a869f2c05a9c5b1c4
Alexander Bayer <alexanderbayer@posteo.de>
2022-12-21 12:05:22 +0100

    Typing errors in form

--- lib/upload.ts
+++ lib/upload.ts
@@ -1,4 +1,4 @@
-export default async function cloudUpload(files) {
+export default async function cloudUpload(files: Blob[]): Promise<string[]> {
   let urls = [];
 
   for (const file of files) {
--- pages/newuser.tsx
+++ pages/newuser.tsx
@@ -4,25 +4,30 @@ import Footer from "../components/Footer";
 import { Artist } from "../lib/ArtistClass";
 import type { ArtistInterface } from "../lib/ArtistClass";
 import cloudUpload from "../lib/upload";
+import React from "react";
 
 export default function ArtistPage(): JSX.Element {
-  async function upload(event) {
+  async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
-    const files = event.target.pics.files;
-    const urls = await cloudUpload(files);
-    console.log(urls);
+
+    const { artistname, firstname, lastname, location, pics } = event.target;
+
+    const urls = await cloudUpload(pics.files);
+
     const newArtist = new Artist(
-      event.target.artistname.value,
-      event.target.firstname.value,
-      event.target.lastname.value,
-      event.target.location.value,
+      artistname.value,
+      firstname.value,
+      lastname.value,
+      location.value,
       urls
     );
+
     const responseDB = await fetch("/api", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newArtist),
     });
+
     console.log(responseDB);
   }
 
@@ -33,7 +38,7 @@ export default function ArtistPage(): JSX.Element {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <form onSubmit={upload}>
+      <form onSubmit={onSubmit}>
         <input name="artistname" placeholder="Name" required />
         <input name="firstname" placeholder="Firstname" />
         <input name="lastname" placeholder="Lastname" />
