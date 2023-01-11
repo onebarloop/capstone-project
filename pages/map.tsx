11993842e11443f6e87dc8b441cc0de9019c7712
Alexander Bayer <alexanderbayer@posteo.de>
2023-01-11 15:37:27 +0100

    User Position is collected and stored in state

--- pages/map.tsx
+++ pages/map.tsx
@@ -3,12 +3,34 @@ import Header from "../components/Header";
 import dynamic from "next/dynamic";
 import Footer from "../components/Footer";
 import { ArtistInterface } from "../lib/ArtistClass";
+import { useEffect, useState } from "react";
 
 type MapProps = {
   artists: ArtistInterface[];
 };
 
+type Position = {
+  lat: number | null;
+  lon: number | null;
+};
+
 export default function MapPage({ artists }: MapProps) {
+  const [userPosition, setUserPosition] = useState<Position>({
+    lat: null,
+    lon: null,
+  });
+
+  console.log(userPosition);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setUserPosition({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+    });
+  }, []);
+
   const Map = dynamic(() => import("../components/Map"), {
     loading: () => <p>A map is loading</p>,
     ssr: false,
