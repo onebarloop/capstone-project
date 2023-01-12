ef77bc08c1b127139154c3b9a755bcf2c5d1f4ae
Alexander Bayer <alexanderbayer@posteo.de>
2023-01-12 09:08:45 +0100

    Pictures have now higher solution

--- components/Picture.tsx
+++ components/Picture.tsx
@@ -6,6 +6,7 @@ type PictureProps = {
   height: number;
   source: string;
   className?: string;
+  big?: boolean;
 };
 
 export default function Picture({
@@ -13,6 +14,7 @@ export default function Picture({
   height,
   source,
   className,
+  big,
 }: PictureProps): JSX.Element {
   return (
     <StyledWrapper className={className}>
@@ -22,6 +24,7 @@ export default function Picture({
         width={width}
         height={height}
         crop="fill"
+        big={big}
       />
     </StyledWrapper>
   );
@@ -36,4 +39,7 @@ const StyledWrapper = styled.div`
 
 const StyledImage = styled(CldImage)`
   border-radius: 0.2em;
+  object-fit: contain;
+  width: ${(props) => (props.big ? "150px" : "125px")};
+  height: ${(props) => (props.big ? "150px" : "125px")};
 `;
